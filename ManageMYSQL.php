602593c32e6048f4bb9760dd6bf992f943cb05c8
Can Gokceaslan <cangokceaslanmail@gmail.com>
2019-06-24 11:26:26 +0300

    v1.0
    v1.0

---
+++ ManageMYSQL.php
@@ -0,0 +1,322 @@
+<?php
+abstract class TableKeys{
+    const tableKeys = ["profile"=>[
+        "id","name","surname","telephone","city","district","address","bday","gender"
+    ]];
+} 
+Class ManageMYSQL extends Export{
+    public function __construct(){
+        $this->host = "localhost";
+        $this->username = "root";
+        $this->password = "root";
+        $this->db = "shopilfy";
+    }
+    public function connect(){
+        $conn_str = 'mysql:host='.$this->host.';dbname='.$this->db;
+        try{
+            $conn = new PDO($conn_str,$this->username,$this->password) or die("WTF");
+            return $conn;
+        }catch (PDOException $e) {
+            echo "<style>body{background:red !important;}</style>";
+            die("<a href='http://google.com/search?q=".$e->getMessage()."' style='color:white;text-decoration: none;'><h2>".$e->getMessage() . "</h2></a><h3 style='color:white;'>Please Correct Your Mistake !!!!!!!</h3>");
+        }
+    }
+    public function check_connection($error = "not"){
+        $conn_str = 'mysql:host='.$this->host.';dbname='.$this->db;
+        try{
+            $conn = new PDO($conn_str,$this->username,$this->password);
+            if($error == "show"){
+                echo "<style>body{background:green !important;}</style>";
+                echo "<h2 style='color:white;'>CONNECTION ESTABLISHED</h2>";
+            }
+            return true;
+        }catch (PDOException $e) {
+            if($error == "show"){
+                echo "<style>body{background:red !important;)</style>";
+                die("<a href='http://google.com/search?q=".$e->getMessage()."' style='color:white;text-decoration: none;'><h2>".$e->getMessage() . "</h2></a>");
+                return false;
+            }else{
+                return false;
+            }
+        }
+    }
+    public function preventAttacks($data){
+        $finished = [];
+        $temp = "";
+        define('CHARSET', 'ISO-8859-1');
+        define('REPLACE_FLAGS', ENT_COMPAT | ENT_XHTML);
+        for($i = 0; $i < count($data); $i++){
+            $temp = htmlspecialchars($data[$i], REPLACE_FLAGS,CHARSET, true);
+            $temp1 = htmlentities($temp,REPLACE_FLAGS,CHARSET, true);
+            if($temp1 == $data[$i]){
+                $temp2 = str_replace(["'",'"'],["\'",'\"'],$temp1);
+                array_push($finished,$temp2);
+            }else{
+                $temp2 = "-";
+                array_push($finished,$temp2);
+            }
+        }
+        return $finished;
+    }
+    public function run($sql,$error="not",$fetch="assoc"){
+        $conn = $this->connect();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $conn = null;
+        if($error == "show"){
+        if($stmt->errorInfo()[0] > 0){
+            echo "<style>body{background:red !important;)</style>";
+            echo "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+        }
+        }
+        if($fetch == "assoc"){
+            $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }else if($fetch == "array"){
+            $data = $stmt->fetchAll();
+        }
+            return ["data"=>$data,"rows"=>$stmt->rowCount()];
+    }
+    public function insert($table,$keys,$values,$error="not"){
+        $conn = $this->connect();
+        $keys_str = "";
+        $values_str = "";
+        $length_keys_str = count($keys);
+        $length_values_str = count($values);
+        if($length_keys_str == $length_values_str){
+        for($i = 0; $i < $length_keys_str; $i++){
+            if($i == 0){
+                $keys_str = "(".$keys[$i];
+                $values_str = "('".$values[$i]."'";
+            }else if($i == $length_keys_str-1){
+                $keys_str = $keys_str.",".$keys[$i].")";
+                $values_str = $values_str.",'".$values[$i]."')";
+                
+            }else{
+                $keys_str = $keys_str.",".$keys[$i];
+                $values_str = $values_str.",'".$values[$i]."'";
+            }
+        }
+        $sql = "INSERT INTO ".$table.$keys_str." VALUES ".$values_str.";";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        if($error == "show"){
+            if($stmt->errorInfo()[0] != 0){
+                print "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+                print "<style>body{background:red !important;)</style>";
+            }
+        }
+        if($stmt->rowCount() == 1){
+            return true;
+        }else{
+            return false;
+        }
+        }else{
+            echo "<style>body{background:red !important;)</style>";
+            die("<h2 style='color:white;'>Error, Please adjust Keys and Values with same length<br/></h2><h3 style='color:white;'>Please Correct Your Mistake !!!!!!!</h3>");
+        }
+    }
+    public function update($table,$keys,$values,$where_str,$error="not"){
+        $conn = $this->connect();
+        $update_str = "";
+        $length_keys_str = count($keys);
+        $length_values_str = count($values);
+        if($length_keys_str == $length_values_str){
+        for($i = 0; $i < $length_keys_str; $i++){
+            if($i == 0 && $length_keys_str != 1){
+                $update_str = $keys[$i]." = '".$values[$i]."', ";
+            }else if($i == $length_keys_str-1){
+                $update_str = $update_str.$keys[$i]." = '".$values[$i]."'";
+            }else{
+                $update_str = $update_str.$keys[$i]." = '".$values[$i]."', ";
+            }
+        }
+        $sql = "UPDATE ".$table." SET ".$update_str." WHERE ".$where_str.";";
+        echo $sql;
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        if($error == "show"){
+            if($stmt->errorInfo()[0] != 0){
+                print "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+                print "<style>body{background:red !important;)</style>";
+            }
+        }
+        if($stmt->rowCount() == 1){
+            return true;
+        }else{
+            return false;
+        }
+        }else{
+            echo "<style>body{background:red !important;)</style>";
+            die("<h2 style='color:white;'>Error, Please adjust Keys and Values with same length<br/></h2><h3 style='color:white;'>Please Correct Your Mistake !!!!!!!</h3>");
+        }
+    }
+    public function select($table,$select_keys,$where_str,$fetch_type,$error="not",$attacks="search"){
+        $sql = "SELECT ".$select_keys." FROM ".$table." WHERE ".$where_str.";";
+        //echo $sql;
+        $conn = $this->connect();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        if($error == "show"){
+            if($stmt->errorInfo()[0] != 0){
+                print "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+                print "<style>body{background:red !important;)</style>";
+            }
+        }
+        if($fetch_type == "assoc"){
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }else if($fetch_type == "array"){
+            $result = $stmt->fetchAll(\PDO::FETCH_NUM);
+        }else{
+            echo "Enter a right Keyword";
+        }
+        //echo $sql;
+        return $result;
+    }
+    public function delete($table,$where_str,$error="not"){
+        $conn = $this->connect();
+        $sql = "DELETE FROM ".$table." WHERE ".$where_str;
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        if($error == "show"){
+            if($stmt->errorInfo()[0] != 0){
+                print "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+                print "<style>body{background:red !important;)</style>";
+            }
+        }
+        if($stmt->rowCount() == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function inner_join($table,$ontable,$joiningtable,$onjoining){
+        $create = $table." INNER JOIN ".$joiningtable." ON ".$table.".".$ontable." = ".$joiningtable.".".$onjoining;
+        return $create;
+    }
+    public function search($table,$column,$search,$where_str,$fetch_type="assoc"){
+        $col_str = "";
+        $length_column = count($column);
+        for($i = 0;$i < $length_column; $i++){
+            if($length_column != 0 && $i < $length_column-1){
+                $col_str = $col_str."LOWER(CONCAT(".$column[$i].")) LIKE LOWER('%".$search."%') OR ";
+            }else{
+                $col_str = $col_str."LOWER(CONCAT(".$column[$i].")) LIKE LOWER('%".$search."%')";
+            }
+        }
+        $where_search_str = $col_str." AND ".$where_str;
+        $conn = $this->connect();
+        $sql = "SELECT * FROM ".$table." WHERE ".$where_search_str.";";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        if($error == "show"){
+            if($stmt->errorInfo()[0] != 0){
+                print "<h2 style='color:white;'>SQLSTATE ERROR CODE [".$stmt->errorInfo()[1]."] <br/>".$stmt->errorInfo()[2]."</h2>";
+                print "<style>body{background:red !important;)</style>";
+            }
+        }
+        if($fetch_type == "assoc"){
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }else if($fetch_type == "array"){
+            $result = $stmt->fetchAll(\PDO::FETCH_NUM);
+        }else{
+            echo "Enter a right Keyword";
+        }
+        return $result;
+    }
+}
+abstract Class Types{
+    const FETCH_ASSOC = 'assoc';
+    const FETCH_ARRAY = 'array';
+    const SHOW_ERROR = 'show';
+}
+Class Export{
+    function __construct(){
+    }
+    public function exportXML($root_element_name,$keys,$data){
+        $file_name = "download.xml";
+        header("Content-Disposition: attachment; filename=$file_name");
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        header("Content-type: text/xml");
+        $this->readyXML($root_element_name,$keys,$data);
+    }
+    public function showXML($root_element_name,$keys,$data){
+        header("Content-type: text/xml");
+        $this->readyXML($root_element_name,$keys,$data);
+    }
+    public function readyXML($root_element_name,$keys,$data){
+        $xml = new SimpleXMLElement("<{$root_element_name}></{$root_element_name}>");
+        $str = "";
+        $count = count($data);
+        if($count >= 1){
+            echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><".$root_element_name.">";
+        for($i = 0; $i < $count; $i++){
+            $str = $str.$this->assocArrayToXML($data[$i],$xml->addChild($keys));
+        }
+            echo $str."</".$root_element_name.">";
+        }else{
+            echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><".$root_element_name.">";
+            $str = $str.$this->assocArrayToXML($data[0],$xml->addChild($keys));
+            echo $str."</".$root_element_name.">";
+        }
+    }
+    private function assocArrayToXML($ar,$xml){
+        $f = create_function('$f,$c,$a',' 
+                foreach($a as $k=>$v) { 
+                    if(is_array($v)) { 
+                        $ch=$c->addChild($k); 
+                        $f($f,$ch,$v); 
+                    } else { 
+                        $c->addChild($k,$v); 
+                    } 
+                }');
+            $f($f,$xml,$ar);
+        return $xml->asXML(); 
+    }
+    public function exportJSON($data){
+        header('Content-Type: application/json');
+        header("Content-Disposition: attachment; filename=$file_name");
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        echo json_encode($data);
+    }
+    public function showJSON($data){
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    }
+    public function exportCSV($keys,$data){
+        header("Content-Type: text/csv");
+        header("Content-Disposition: attachment; filename=$file_name");
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        $this->outputCSV($keys,$data);
+    }
+    public function showCSV($keys,$values){
+        header('Content-type: text/plain');
+        $data = array_merge($keys,$values);
+        foreach ($data as $row) {
+            $numItems = count($row);
+            $i = 0;
+            foreach($row as $veri){
+                if(++$i === $numItems){
+                    echo $veri."\n";
+                }else{
+                    echo $veri.",";
+                }
+                
+            }
+        }
+    }
+    public function outputCSV($keys,$values,$file_name = 'file.csv') {
+         $data = array_merge($keys,$values);
+         $output = fopen("php://output", "w");
+         foreach ($data as $row) {
+             fputcsv($output, $row);
+         }
+         fclose($output);
+     }
+}
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+require('./ManageMYSQL.php');
+abstract Class TableNames{
+    const tables = array("person","products","images");
+    const table_keys = array(
+        "person"=>["id","name","surname","number","date"],
+        "products"=>["id","name","price","image","description","stock","color"],
+        "images"=>["id","iimage","ikey"]
+    );
+}
+$manage = new ManageMYSQL("127.0.0.1:3306","root","root","example");
+// $checkConnection = $manage->check_connection(Types::SHOW_ERROR);
+// $sql = "INSERT INTO person(id,name,surname,number,date) VALUES (21,'Can','Gokceaslan','05444850586','2019-09-12');";
+// $inner_join = $manage->inner_join(TableNames::tables[2],"product_id",TableNames::tables[1],"id");
+// $sql = "SELECT * FROM person WHERE 1";
+// $result = $manage->run($sql,$assoc=Types::FETCH_ASSOC);
+// print_r($result);
+//$result = $manage->search("person",TableNames::table_keys["person"],"Can","1");
+// $manage->showXML("Data","element",$result);
+//$manage->showJSON($result);
+// $exportjson = $manage->exportCSV(Array(TableNames::table_keys["person"]),$result);
+// $showxml = $manage->showJSON($result);
+// $variable = $manage->preventAttacks(["<section>BASIUDFHASDUH</section>","<script>alert('Can')</script>","<script>","'","CAN"]);
+// $showxml = $manage->showJSON($variable);
+// $is_done = $manage->insert(TableNames::tables[0],TableNames::table_keys["person"],[32,"Can","Gokceaslan","05230000000","2019-06-29"],Types::SHOW_ERROR);
+// $is_done = $manage->update(TableNames::tables[0],TableNames::table_keys["person"],[550,"Can","Amcan","05442342523","2019-10-11"],"id=32",Types::SHOW_ERROR);
+/* $is_done = $manage->delete(TableNames::tables[0],"id=550",Types::SHOW_ERROR);
+if($is_done){
+    echo "DONE";
+}else{
+    echo "ERROR";
+} */
+// $where = $manage->preventAttacks(["1",'"tryout"','<a href="./can.php">tryout</a>']);
+// $data = $manage->select(TableNames::tables[0],"*","1",Types::FETCH_ASSOC,Types::SHOW_ERROR);
+// $data = [["id"=>"1","name"=>"Can","surname"=>"Gökçeaslan","photos"=>["long"=>"https://cangokceaslan.github.io/images/cangokceaslan-long.jpg","normal"=>"https://cangokceaslan.github.io/images/cangokceaslan.jpg"]]];
+// $showxml = $manage->showXML("Data","person",$data);
+// $showjson = $manage->exportCSV(Array(TableNames::table_keys["person"]),$data);
+// $showcsv = $manage->showCSV(Array(TableNames::table_keys["person"]),$data);
+// $exportxml = $manage->exportXML("Can","cancan",$data);
+// $exportjson = $manage->exportJSON($data);
+// $exportcsv = $manage->exportCSV(Array(TableNames::table_keys["person"]),$data);
+// if($is_done){
+//     echo "Done";
+// }else{
+//     echo "Not Done";
+// }
+//$manage->showJSON($data);
+?>
